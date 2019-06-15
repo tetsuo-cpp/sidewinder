f3f8d32306f4894f48caa32b449da3f0286b65ef
Alex Cameron <alex.cameron@10gen.com>
2019-06-15 22:36:31 +1000

    Get rid of unnecessary forward decl.

--- Interfaces.h
+++ Interfaces.h
@@ -20,19 +20,17 @@ public:
   virtual void stop() = 0;
 };
 
-class IConnection;
+class IConnection {
+public:
+  virtual ~IConnection() = default;
+  virtual void sendData(const char *data, int len) = 0;
+};
 
 class IServerHandler {
 public:
   virtual ~IServerHandler() = default;
   virtual void onConnection(std::unique_ptr<IConnection> conn) = 0;
   virtual bool handleData(const char *data, int len, IConnection *conn) = 0;
-};
-
-class IConnection {
-public:
-  virtual ~IConnection() = default;
-  virtual void sendData(const char *data, int len) = 0;
 };
 
 } // namespace sidewinder
